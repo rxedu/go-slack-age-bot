fd4939cca3e258b2cb453d68d64e21a728ef0d99
Evan Sosenko <razorx@evansosenko.com>
2022-09-03 12:54:03 -0500

    Add age command

--- internal/commands.go
+++ internal/commands.go
@@ -1,5 +1,31 @@
 package internal
 
-import "github.com/shomali11/slacker"
+import (
+	"fmt"
+	"strconv"
 
-func RegisterCommands(bot *slacker.Slacker) {}
+	"github.com/shomali11/slacker"
+)
+
+func RegisterCommands(bot *slacker.Slacker) {
+	bot.Command("I was born in <year>", &slacker.CommandDefinition{
+		Description: "About how old are you?",
+		Examples:    []string{"I was born in <year>"},
+		Handler:     yobHandler,
+	})
+}
+
+func yobHandler(ctx slacker.BotContext, r slacker.Request, w slacker.ResponseWriter) {
+	year := r.Param("year")
+	yob, err := strconv.Atoi(year)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	age := 2021 - yob
+	msg := fmt.Sprintf("You are about %d years old.", age)
+	err = w.Reply(msg)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
